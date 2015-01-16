a6dabe822b71a9d43d2e680bc9284b96d451b9d3
M0xkLurk3r <lkphantom1995@gmail.com>
2015-01-17 00:22:12 +0800

    编写了HTTP访问组件（未完成）以及用户验证（未完成）。等待http接口

--- app/src/main/java/boom/boom/api/User.java
+++ app/src/main/java/boom/boom/api/User.java
@@ -1,7 +1,37 @@
 package boom.boom.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by 1eekai on 2015/1/16.
  */
 public class User {
+    private String username;
+    private String password;
+    private boolean ifUserLoggedIn;
+    private Map<String, String> userData;
+    public User(String _username, String _password){
+        this.username = _username;
+        this.password = _password;
+        AttemptLogin();
+        SyncUserData();
+    }
+
+    public boolean SyncUserData(){
+        userData = new HashMap<String, String>();
+        return true;
+
+    }
+
+    public boolean ifLoggedIn(){
+        return this.ifUserLoggedIn;
+    }
+
+    public boolean AttemptLogin(){
+
+        return this.ifLoggedIn();
+    }
+
+ //   public String
 }
