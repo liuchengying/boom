02ab5edd72646f4175b90f4466293b22d5228598
M0xkLurk3r <lkphantom1995@gmail.com>
2015-01-17 00:22:32 +0800

    编写了HTTP访问组件（未完成）以及用户验证（未完成）。等待http接口

--- app/src/main/java/boom/boom/api/HttpIO.java
+++ app/src/main/java/boom/boom/api/HttpIO.java
@@ -1,7 +1,97 @@
 package boom.boom.api;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.List;
+
 /**
  * Created by 1eekai on 2015/1/16.
  */
 public class HttpIO {
+    public String URLstr;
+    public HttpIO(String ur1){
+        this.URLstr = ur1;
+    }
+    public String ResultBuffer;
+    public String Result;
+    public String POSTToHTTPServer(List<NameValuePair> postParameters) {
+        String result = null;
+        BufferedReader reader = null;
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpPost request = new HttpPost();
+            request.setURI(new URI(this.URLstr));
+            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(
+                    postParameters);
+            request.setEntity(formEntity);
+
+            HttpResponse response = client.execute(request);
+            reader = new BufferedReader(new InputStreamReader(response
+                    .getEntity().getContent()));
+
+            StringBuffer strBuffer = new StringBuffer("");
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                strBuffer.append(line);
+            }
+            result = strBuffer.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                    reader = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+    public String GETToHTTPServer() {
+        String result = null;
+        BufferedReader reader = null;
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet();
+            request.setURI(new URI(
+                    this.URLstr));
+            HttpResponse response = client.execute(request);
+            reader = new BufferedReader(new InputStreamReader(response
+                    .getEntity().getContent()));
+
+            StringBuffer strBuffer = new StringBuffer("");
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                strBuffer.append(line);
+            }
+            result = strBuffer.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                    reader = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
+    }
 }
