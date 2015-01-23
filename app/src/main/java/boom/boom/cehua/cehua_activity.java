97ebf23cb9dac6ffeee80de86b13cf56df232ac6
lyp741 <920511642@qq.com>
2015-01-22 23:09:46 -0800

    在Tiaozhan_activity添加侧滑菜单功能 添加信息中心和好友列表（通讯录）activity By Lyp

--- app/src/main/java/boom/boom/cehua/cehua_activity.java
+++ app/src/main/java/boom/boom/cehua/cehua_activity.java
@@ -1,13 +1,21 @@
 package boom.boom.cehua;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import boom.boom.xinxizhongxin.xinxizhongxin_activity;
 import boom.boom.R;
-
+import boom.boom.tongxunlu.tongxunlu_activity;
+import boom.boom.shezhi.Shezhi_activity;
 /**
  * Created by 刘成英 on 2015/1/14.
  */
+
 public class cehua_activity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/boom/boom/tianzhan/Tiaozhan_activity.java
+++ app/src/main/java/boom/boom/tianzhan/Tiaozhan_activity.java
@@ -5,12 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import boom.boom.R;
 import boom.boom.guizejieshao.Guizejieshao_activity;
 import boom.boom.shezhi.Shezhi_activity;
 import boom.boom.slidingMenu.SlidingMenu;
-
+import boom.boom.xinxizhongxin.xinxizhongxin_activity;
+import boom.boom.tongxunlu.tongxunlu_activity;
 /**
  * Created by 刘成英 on 2015/1/13.
  */
@@ -45,6 +49,54 @@ public class Tiaozhan_activity extends Activity {
                 startActivity(intent);
             }
         });
+
+        LinearLayout xxzx=(LinearLayout)findViewById(R.id.ch_xinxizhongxin);
+        xxzx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent();
+                myIntent.setClass(Tiaozhan_activity.this,xinxizhongxin_activity.class);    //第一个参数不知道对不对
+                Tiaozhan_activity.this.startActivity(myIntent);
+            }
+        });                                                                              //信息中心
+
+        LinearLayout hylb=(LinearLayout)findViewById(R.id.ch_haoyouliebiao);
+        hylb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent();
+                myIntent.setClass(Tiaozhan_activity.this,tongxunlu_activity.class);
+                Tiaozhan_activity.this.startActivity(myIntent);
+            }
+        });                                                                             //好友列表->通讯录
+
+        LinearLayout jfsc=(LinearLayout)findViewById(R.id.ch_jifenshangcheng);
+        jfsc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Tiaozhan_activity.this, "此功能尚未实现，敬请期待！",Toast.LENGTH_SHORT).show();
+            }
+        });                                                                             //积分商城
+
+        LinearLayout phb=(LinearLayout)findViewById(R.id.ch_paihangbang);
+        phb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Tiaozhan_activity.this, "此功能尚未实现，敬请期待！", Toast.LENGTH_SHORT).show();
+            }
+        });                                                                             //排行榜
+
+        LinearLayout xtsz=(LinearLayout)findViewById(R.id.ch_xitongshezhi);
+        xtsz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent();
+                myIntent.setClass(Tiaozhan_activity.this,Shezhi_activity.class);
+                Tiaozhan_activity.this.startActivity(myIntent);
+            }
+        });                                                                             //系统设置->设置
+
+
     }
     public void dianjicehua(View view){
         mLeftMenu.toggle();
