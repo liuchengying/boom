8e20ee7bec99251bd08a1ad8bbeb2449011c508b
lyp741 <920511642@qq.com>
2015-01-22 23:12:43 -0800

    在Tiaozhan_activity添加侧滑菜单功能 添加信息中心和好友列表（通讯录）activity By Lyp

---
+++ app/src/main/java/boom/boom/tongxunlu/tongxunlu_activity.java
@@ -0,0 +1,18 @@
+package boom.boom.tongxunlu;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import boom.boom.R;
+/**
+ * Created by Administrator on 2015/1/22.
+ */
+public class tongxunlu_activity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tongxunlu);
+    }
+}
---
+++ app/src/main/java/boom/boom/xinxizhongxin/xinxizhongxin_activity.java
@@ -0,0 +1,29 @@
+package boom.boom.xinxizhongxin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import boom.boom.R;
+
+/**
+ * Created by Lyp on 2015/1/22
+ */
+
+public class xinxizhongxin_activity extends Activity {
+    @Override
+      protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xinxizhongxin);
+        Button fh=(Button)findViewById(R.id.xxzx_fanhui);
+        fh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
