f2125175c3c201013d55369bdb0b66009fc85467
M0xkLurk3r <lkphantom1995@gmail.com>
2015-02-19 20:09:12 +0800

    移动全局变量到静态类中，JSONObject全局化处理。

---
+++ app/src/main/java/boom/boom/api/Static.java
@@ -0,0 +1,19 @@
+package boom.boom.api;
+
+import org.json.JSONObject;
+
+/**
+ * Created by laoli on 2015/2/19.
+ */
+public class Static {
+    public static String session_id = null;
+    public static String username = null;
+    public static String nickname = null;
+    public static int logintime;
+    public static int coins;
+    public static String avatar = null;
+    public static String uniqueSign = null;
+    public static String reg_date = null;
+    public static JSONObject object = null;
+    public static String tempStr = null;
+}
