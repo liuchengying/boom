c5f27a802c18c0395bdefb39099524f190593ea0
M0xkLurk3r <lkphantom1995@gmail.com>
2015-02-20 10:06:06 +0800

    设计收信功能

--- app/src/main/java/boom/boom/api/Msg.java
+++ app/src/main/java/boom/boom/api/Msg.java
@@ -1,10 +1,50 @@
 package boom.boom.api;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by laoli on 2015/2/19.
  */
 public class Msg {
+    public static String MSG_API_URL = "/api/msg.php";
+    private String RawDataStore;
+    private JSONObject json_data;
+    private static int counter = 0;
     public Msg(){
+        HttpIO io = new HttpIO(Utils.serveraddr + MSG_API_URL + "?action=query");
+        io.SetCustomSessionID(Static.session_id);
+        io.GETToHTTPServer();
+        RawDataStore = io.getResultData();
+        try {
+            json_data = new JSONObject(RawDataStore);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public Map<String, Object> GetSimpleMap(boolean reset){
+        if (reset == true)  counter = 0;
+        try {
+            JSONObject tmp = Utils.GetSubJSONObject(json_data, "" + counter + "");
+            if (tmp.toString() == "") {
 
+                return null;
+            } else if (tmp == null) {
+                return null;
+            }
+            Map<String,Object>  map = new HashMap<String,Object>();
+            map.put("label", tmp.getString("title"));
+            map.put("text", tmp.getString("text"));
+            return map;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public Map<String, Object>  GetSimpleMap(){
+        return this.GetSimpleMap(false);
     }
 }
--- app/src/main/java/boom/boom/xinxizhongxin/xinxizhongxin_activity.java
+++ app/src/main/java/boom/boom/xinxizhongxin/xinxizhongxin_activity.java
@@ -1,31 +1,60 @@
 package boom.boom.xinxizhongxin;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import boom.boom.R;
+import boom.boom.api.Msg;
 
 /**
  * Created by Lyp on 2015/1/22
  */
 
 public class xinxizhongxin_activity extends Activity {
-    @Override
+      private ListView list;
       protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xinxizhongxin);
         Button fh=(Button)findViewById(R.id.xxzx_fanhui);
-        Button rightSideButton;
+//        Button rightSideButton;
+        final ArrayList<Map<String, Object>> listItem = new ArrayList<>();
+        list = (ListView)findViewById(R.id.challenge_list);
+        while (true) {
+            Msg msg = new Msg();
+            Map<String, Object> map = new HashMap<String, Object>();
+            map = msg.GetSimpleMap();
+            if (map == null) {
+                break;
+            }
+            listItem.add(map);
+        }
+        SimpleAdapter listItemAdapter = new SimpleAdapter(this,listItem,
+                  R.layout.msgcenter_listitem,
+                  new String[] {"label", "text"},
+                  new int[] {R.id.msg_center_list_label, R.id.msg_center_list_text}
+        );
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "删除、回信功能尚未实现，敬请期待。", Toast.LENGTH_SHORT);
+                return false;
+            }
+        });
         fh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-
     }
 }
