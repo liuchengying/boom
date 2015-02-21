766a4fb9dd05e2631504674fa438646b8450e313
M0xkLurk3r <lkphantom1995@gmail.com>
2015-02-21 22:50:47 +0800

    例行更新。

--- app/src/main/java/boom/boom/paishetiaozhan/Paishetiaozhan_activity.java
+++ app/src/main/java/boom/boom/paishetiaozhan/Paishetiaozhan_activity.java
@@ -2,8 +2,11 @@ package boom.boom.paishetiaozhan;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import java.util.Timer;
@@ -20,25 +23,26 @@ import boom.boom.tingzhitiaozhan.Tingzhipaishe_activity;
 public class Paishetiaozhan_activity extends Activity {
     private Button paishefanhui;
     private Button kaishipaishe;
-
+    private SurfaceView sv;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);
         setContentView(R.layout.paishetiaozhan);
         paishefanhui = (Button) findViewById(R.id.paishefanhui);
         kaishipaishe = (Button) findViewById(R.id.kaishipaishe);
+        sv = (SurfaceView) findViewById(R.id.challengerecord_surface);
         kaishipaishe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(Paishetiaozhan_activity.this,Tingzhipaishe_activity.class);
                 startActivity(intent);
-
-
-
             }
         });
         paishefanhui.setOnClickListener(new View.OnClickListener() {
