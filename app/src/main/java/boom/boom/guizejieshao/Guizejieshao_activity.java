de436d9f2e0d80c46d3842039517afc401e91dbd
M0xkLurk3r <lkphantom1995@gmail.com>
2015-02-21 13:21:23 +0800

    网络视频流处理

--- app/src/main/java/boom/boom/guizejieshao/Guizejieshao_activity.java
+++ app/src/main/java/boom/boom/guizejieshao/Guizejieshao_activity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import boom.boom.R;
 import boom.boom.paishetiaozhan.Paishetiaozhan_activity;
@@ -16,12 +17,15 @@ import boom.boom.tianzhan.Tiaozhan_activity;
 public class Guizejieshao_activity extends Activity{
     private Button fanhuitianzhan;
     private  Button woyaotianzhan;
+    private VideoView frame_frontvideo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.guizejieshao);
         fanhuitianzhan = (Button) findViewById(R.id.fanhuitiaozhan);
         woyaotianzhan = (Button) findViewById(R.id.woyaotiaozhan);
+        frame_frontvideo = (VideoView) findViewById(R.id.videoView_front);
+//        frame_frontvideo.
         woyaotianzhan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)  {
