a6473fc288d5f647c30b7e3e7581f05061bc26f1
lyp741 <920511642@qq.com>
2015-03-10 21:04:13 +0800

    网络多线程解决网络问题

--- app/src/main/java/boom/boom/tianzhan/Tiaozhan_activity.java
+++ app/src/main/java/boom/boom/tianzhan/Tiaozhan_activity.java
@@ -8,7 +8,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -17,7 +19,7 @@ import boom.boom.api.Static;
 import boom.boom.guizejieshao.Guizejieshao_activity;
 import boom.boom.shezhi.Shezhi_activity;
 import boom.boom.slidingMenu.SlidingMenu;
-
+import boom.boom.xinxizhongxin.xinxizhongxin_activity;
 /**
  * Created by 刘成英 on 2015/1/13.
  */
@@ -66,7 +68,15 @@ public class Tiaozhan_activity extends Activity {
         coins = (TextView) findViewById(R.id.mycoins);
         nickname.setText(user_nickname);
         coins.setText("积分：" + user_coins);
-
+        LinearLayout xxzx=(LinearLayout)findViewById(R.id.ch_xinxizhongxin);
+        xxzx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(Tiaozhan_activity.this, xinxizhongxin_activity.class);
+                startActivity(intent);
+            }
+        });
     }
     public void dianjicehua(View view){
         mLeftMenu.toggle();
