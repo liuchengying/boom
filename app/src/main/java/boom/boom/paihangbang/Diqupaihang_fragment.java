873a50c15616eaa84d99282fe7c9361140c557ce
lcy363663627 <363663627@qq.com>
2015-03-16 20:19:37 +0800

    家listview

--- app/src/main/java/boom/boom/paihangbang/Diqupaihang_fragment.java
+++ app/src/main/java/boom/boom/paihangbang/Diqupaihang_fragment.java
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import boom.boom.R;
 
@@ -17,6 +22,23 @@ public class Diqupaihang_fragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.paihangbang1, container, false);
+        View v=inflater.inflate(R.layout.paihangbang1, container, false);
+        ListView lv=(ListView)v.findViewById(R.id.listView4);
+        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String,     Object>>();//*在数组中存放数据*//*
+        for(int i=0;i<10;i++)
+        {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("title","XXX");//加入图片            map.put("ItemTitle", "第"+i+"行");
+            map.put("count", "这是第"+i+"行");
+            listItem.add(map);
+        }
+        SimpleAdapter mSimpleAdapter = new SimpleAdapter(getActivity(),listItem,//需要绑定的数据
+                R.layout.diqupaihang_item,//每一行的布局//动态数组中的数据源的键对应到定义布局的View中
+                new String[] {
+                        "title", "count"},
+                new int[] {R.id.title,R.id.count}
+        );
+        lv.setAdapter(mSimpleAdapter);
+        return v;
     }
 }
