f59e2a1a9312fbf8963353da5d9acf02a979b37e
lcy363663627 <363663627@qq.com>
2015-03-17 20:29:32 +0800

    Fuck the git

--- app/src/main/java/boom/boom/shezhi/Shezhi_activity.java
+++ app/src/main/java/boom/boom/shezhi/Shezhi_activity.java
@@ -3,14 +3,25 @@ package boom.boom.shezhi;
 
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.ToggleButton;
 
 import boom.boom.R;
-import boom.boom.tianzhan.Tiaozhan_activity;
 
 /**
  * Created by 刘成英 on 2015/1/15.
@@ -29,5 +40,45 @@ public class Shezhi_activity extends Activity{
                 finish();
             }
         });
+        final ToggleButton mTogBtn = (ToggleButton) findViewById(R.id.sz_toggle1);
+        final ToggleButton mTogBtn1 = (ToggleButton) findViewById(R.id.sz_toggle2);
+        final ToggleButton mTogBtn2 = (ToggleButton) findViewById(R.id.sz_toggle3);// 获取到控件
+        mTogBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                // TODO Auto-generated method stub
+                if (isChecked) {
+                //isChecken = ture   条件下的操作
+                }else{
+                    //isChecken = ture
+                }
+            }
+        });
+        mTogBtn1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                // TODO Auto-generated method stub
+                if (isChecked) {
+                    //isChecken = ture   条件下的操作
+                }else{
+                    //isChecken = ture
+                }
+            }
+        });
+        mTogBtn2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                // TODO Auto-generated method stub
+                if (isChecked) {
+                    //isChecken = ture   条件下的操作
+                }else{
+                    //isChecken = ture
+                }
+            }
+        });
+
     }
 }
