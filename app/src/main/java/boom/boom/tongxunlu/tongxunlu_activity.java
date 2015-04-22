2d4bd328345e966697dcab0635773cfdf7998db3
lyp741 <920511642@qq.com>
2015-04-22 20:43:16 +0800

    通讯录

--- app/src/main/java/boom/boom/tongxunlu/tongxunlu_activity.java
+++ app/src/main/java/boom/boom/tongxunlu/tongxunlu_activity.java
@@ -1,9 +1,13 @@
 package boom.boom.tongxunlu;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CheckedTextView;
@@ -11,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,7 +43,6 @@ public class tongxunlu_activity extends Activity{
         SildingFinishLayout mSildingFinishLayout = (SildingFinishLayout) findViewById(R.id.sildingFinishLayout);
         mSildingFinishLayout
                 .setOnSildingFinishListener(new SildingFinishLayout.OnSildingFinishListener() {
-
                     @Override
                     public void onSildingFinish() {
                         tongxunlu_activity.this.finish();
@@ -50,22 +54,25 @@ public class tongxunlu_activity extends Activity{
         FontManager.changeFonts(FontManager.getContentView(this), this);//字体
         ListView lv = (ListView) findViewById(R.id.tongxunlu_listview);
 
-        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String,     Object>>();
+        ArrayList<String> listItem = new ArrayList<>();
         for(int i=0;i<30;i++)
         {
-            HashMap<String, Object> map = new HashMap<String, Object>();
-            map.put("picther",R.drawable.android_181);//加入图片            map.put("ItemTitle", "第"+i+"行");
-            map.put("nicheng", "好友昵称");
-            listItem.add(map);
+            listItem.add("老"+i);
         }
-        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this,listItem,//需要绑定的数据
+        final MyAdapter mSimpleAdapter=new MyAdapter(listItem,this);
+       /* SimpleAdapter mSimpleAdapter = new SimpleAdapter(this,listItem,//需要绑定的数据
                 R.layout.tongxunlu_listview_item,//每一行的布局//动态数组中的数据源的键对应到定义布局的View中
                 new String[] {
                         "picther", "nicheng"},
                 new int[] {R.id.tongxunlu_touxiang,R.id.tongxunlu_nicheng}
-        );
+        );*/
         lv.setAdapter(mSimpleAdapter);
-
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mSimpleAdapter.select(position);
+            }
+        });
 
         LinearLayout fanhui= (LinearLayout) findViewById(R.id.txl_fh);
         fanhui.setOnClickListener(new View.OnClickListener() {
@@ -86,5 +93,85 @@ public class tongxunlu_activity extends Activity{
     }
 
 
+    public class MyAdapter extends BaseAdapter {
+        // 填充数据的list
+        private ArrayList<String> list;
+        // 用来控制CheckBox的选中状况
+        private HashMap<Integer,Boolean> isSelected;
+        // 上下文
+        private Context context;
+        // 用来导入布局
+        private LayoutInflater inflater = null;
 
+        // 构造器
+        public MyAdapter(ArrayList<String> list, Context context) {
+            this.context = context;
+            this.list = list;
+            inflater = LayoutInflater.from(context);
+            isSelected = new HashMap<Integer, Boolean>();
+            // 初始化数据
+            initDate();
+        }
+
+        // 初始化isSelected的数据
+        private void initDate(){
+            for(int i=0; i<list.size();i++) {
+                getIsSelected().put(i,false);
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                // 获得ViewHolder对象
+                holder = new ViewHolder();
+                // 导入布局并赋值给convertview
+                convertView = inflater.inflate(R.layout.tongxunlu_listview_item, null);
+                holder.tv = (TextView) convertView.findViewById(R.id.tongxunlu_nicheng);
+                holder.cb = (CheckBox) convertView.findViewById(R.id.tongxunlu_check);
+                // 为view设置标签
+                convertView.setTag(holder);
+            } else {
+                // 取出holder
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+
+            // 设置list中TextView的显示
+            holder.tv.setText(list.get(position));
+            // 根据isSelected来设置checkbox的选中状况
+            holder.cb.setChecked(getIsSelected().get(position));
+            return convertView;
+        }
+
+        public HashMap<Integer,Boolean> getIsSelected() {
+            return isSelected;
+        }
+
+        public void select(int position) {
+            getIsSelected().put(position,!getIsSelected().get(position));
+            notifyDataSetChanged();
+        }
+class ViewHolder
+{
+            TextView tv;
+            CheckBox cb;
 }
+    }//MyAdapter
+}//tongxunlu_activity
